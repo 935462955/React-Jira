10908c5598d512c3ec60a0a1a0ca3ebbdcaf4a16
935462955 <935462955@qq.com>
2021-04-05 14:36:58 +0800

    chore 安装devtool

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {loadDevTools} from 'jira-dev-tool' //基于service worker的后端代理服务 项目右下角的齿轮 ，无需再运行json-server
 
-ReactDOM.render(
+loadDevTools(() => ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+))
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
